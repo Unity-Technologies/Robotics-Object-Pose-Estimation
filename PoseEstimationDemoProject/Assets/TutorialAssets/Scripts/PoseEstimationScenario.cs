0fbbf9d5eb72044427a72b52b15f0c865d923a37
at669 <amanda.trang@unity3d.com>
2021-03-03 12:22:51 -0700

    Manual dev->main

--- PoseEstimationDemoProject/Assets/TutorialAssets/Scripts/PoseEstimationScenario.cs
+++ PoseEstimationDemoProject/Assets/TutorialAssets/Scripts/PoseEstimationScenario.cs
@@ -1,12 +1,15 @@
+using System;
 using UnityEngine.Perception.Randomization.Scenarios;
 
+[Serializable]
 public class PoseEstimationScenarioConstants : ScenarioConstants
 {
     public int totalFrames = 1000;
 }
+
 public class PoseEstimationScenario : Scenario<PoseEstimationScenarioConstants>
 {
-    public bool automaticIteration = false;
+    public bool automaticIteration = true;
     
     bool shouldIterate = false; 
 
